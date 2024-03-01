adef7679cf764a24c9bb79cb39030cd245c8f407
Russell Shingleton <reshingleton@gmail.com>
2024-03-01 15:35:32 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+
+# NAME
+
+Proc::Orchestrator - It's new $module
+
+# SYNOPSIS
+
+    use Proc::Orchestrator;
+
+# DESCRIPTION
+
+Proc::Orchestrator is ...
+
+# LICENSE
+
+Copyright (C) Russell Shingleton.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Russell Shingleton <reshingleton@gmail.com>
+# Proc-Orchestrator
---
+++ lib/Proc/Orchestrator.pm
@@ -0,0 +1,39 @@
+package Proc::Orchestrator;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Proc::Orchestrator - It's new $module
+
+=head1 SYNOPSIS
+
+    use Proc::Orchestrator;
+
+=head1 DESCRIPTION
+
+Proc::Orchestrator is ...
+
+=head1 LICENSE
+
+Copyright (C) Russell Shingleton.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Russell Shingleton E<lt>reshingleton@gmail.comE<gt>
+
+=cut
+
